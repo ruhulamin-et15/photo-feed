bd4833c9632c9f1417b9ed312141b1d22015918f
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-04-02 23:58:00 +0600

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,29 +1,76 @@
+// import { match } from "@formatjs/intl-localematcher";
+// import Negotiator from "negotiator";
+// import { NextResponse } from "next/server";
+
+// let locales = ["bn", "en"];
+// let defaultLocale = "en";
+
+// function getLocale(request) {
+//   const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+//   let headers = { "accept-language": acceptedLanguage };
+//   let languages = new Negotiator({ headers }).languages();
+//   return match(languages, locales, defaultLocale);
+// }
+
+// export function middleware(request) {
+//   const pathname = request.nextUrl.pathname;
+
+//   const pathnameIsMissingLocale = locales.every(
+//     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+//   );
+
+//   if (pathnameIsMissingLocale) {
+//     const locale = getLocale(request);
+
+//     return NextResponse.redirect(
+//       new URL(`/${locale}/${pathname}`, request.url) // request.url = domain pathname
+//     );
+//   }
+// }
+
+// export const config = {
+//   matcher: [
+//     // Skip all internal paths (_next, assets, api)
+//     "/((?!api|assets|.*\\..*|_next).*)",
+//     // Optional: only run on root (/) URL
+//     // '/'
+//   ],
+// };
+
 import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 import { NextResponse } from "next/server";
 
-let locales = ["bn", "en"];
 let defaultLocale = "en";
+let locales = ["bn", "en"];
 
+// Get the preferred locale, similar to above or using a library
 function getLocale(request) {
   const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
   let headers = { "accept-language": acceptedLanguage };
   let languages = new Negotiator({ headers }).languages();
-  return match(languages, locales, defaultLocale);
+
+  console.log(languages);
+
+  return match(languages, locales, defaultLocale); // -> 'en-US'
 }
 
 export function middleware(request) {
+  // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
 
   const pathnameIsMissingLocale = locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
+  // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
 
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
     return NextResponse.redirect(
-      new URL(`/${locale}/${pathname}`, request.url) // request.url = domain pathname
+      new URL(`/${locale}/${pathname}`, request.url)
     );
   }
 }
